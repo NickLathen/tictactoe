867ab911d6f5034fe17fa57a2f2b1b134534b1d7
NickLathen <nick.lathen@gmail.com>
2016-12-05 15:13:25 -0800

    Finish game logic

---
+++ tictactoe.js
@@ -0,0 +1,101 @@
+const Board = function Board() {
+  this.board = [['','',''],['','',''],['','','']]; // row1 row2 row3
+}
+
+const areEqual = function areEqual(a, b, c) {
+  return a === b && b === c;
+};
+
+Board.prototype = {
+  drawBoard: function drawBoard() {
+    console.log(`${this.board[0][0] || ' '}|${this.board[0][1] || ' '}|${this.board[0][2] || ' '}`);
+    console.log('______');
+    console.log(`${this.board[1][0] || ' '}|${this.board[1][1] || ' '}|${this.board[1][2] || ' '}`);
+    console.log('______');
+    console.log(`${this.board[2][0] || ' '}|${this.board[2][1] || ' '}|${this.board[2][2] || ' '}`);
+  },
+  checkBoard: function checkBoard(cb) {
+    if (areEqual(this.board[0][0], this.board[0][1], this.board[0][2]) && this.board[0][0]) {
+      if (this.board[0][0] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[1][0], this.board[1][1], this.board[1][2]) && this.board[1][0]) {
+      if (this.board[1][0] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[2][0], this.board[2][1], this.board[2][2]) && this.board[2][0]) {
+      if (this.board[2][0] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[0][0], this.board[1][0], this.board[2][0]) && this.board[0][0]) {
+      if (this.board[0][0] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[0][1], this.board[1][1], this.board[2][1]) && this.board[0][1]) {
+      if (this.board[0][0] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[0][2], this.board[1][2], this.board[2][2]) && this.board[0][2]) {
+      if (this.board[0][2] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[0][0], this.board[1][1], this.board[2][2]) && this.board[0][0]) {
+      if (this.board[0][0] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else if (areEqual(this.board[0][2], this.board[1][1], this.board[2][0]) && this.board[0][2]) {
+      if (this.board[0][2] === 'X') {
+        console.log('player 1 wins');
+      } else {
+        console.log('player 2 wins')
+      }
+    } else {
+      cb();
+    }
+  }
+};
+
+const board = new Board();
+console.log('Player 1, enter your turn by typing playX(row, column)');
+
+
+playX = function playX(row, column) {
+  if (board.board[row][column] !== '') {
+    console.log('invalid play, try again');
+    return null;
+  } else {
+    board.board[row][column] = 'X';
+  }
+  board.checkBoard(function() {
+    console.log('Player 2, enter your turn by typing playO(row, column)');
+  });
+};
+
+playO = function playO(row, column) {
+  if (board.board[row][column] !== '') {
+    console.log('invalid play, try again');
+    return null;
+  } else {
+    board.board[row][column] = 'Y';
+  }
+  board.checkBoard(function() {
+    console.log('Player 1, enter your turn by typing playX(row, column)');
+  });
+};
+
+//test
+board.drawBoard();
